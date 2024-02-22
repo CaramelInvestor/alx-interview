b0776ac61a68c4741c51987641489aa83939b75b
CaramelInvestor <caramelInvestor@gmail.com>
2024-02-22 22:50:16 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,18 +10,52 @@ def minOperations(n):
     Returns:
         Integer : if n is impossible to achieve, return 0
     """
-    if n <= 1:
-        return n
+    pasted_chars = 1  # how many chars in the file
+    clipboard = 0  # how many H's copied
+    counter = 0  # operations counter
 
-    operations = 0
-    clipboard = 1  # Number of characters in the clipboard
-    buffer = 1     # Number of characters currently in the text editor
+    while pasted_chars < n:
+        # if did not copy anything yet
+        if clipboard == 0:
+            # copyall
+            clipboard = pasted_chars
+            # increment operations counter
+            counter += 1
 
-    while buffer < n:
-        if n % buffer == 0:  # If 'n' is divisible by current buffer size, perform Copy All
-            clipboard = buffer
-            operations += 1
-        buffer += clipboard  # Perform Paste
-        operations += 1
+        # if haven't pasted anything yet
+        if pasted_chars == 1:
+            # paste
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 1
+            # continue to next loop
+            continue
 
-    return operations
+        remaining = n - pasted_chars  # remaining chars to Paste
+        # check if impossible by checking if clipboard
+        # has more than needed to reach the number desired
+        # which also means num of chars in file is equal
+        # or more than in the clipboard.
+        # in both situations it's impossible to achieve n of chars
+        if remaining < clipboard:
+            return 0
+
+        # if can't be devided
+        if remaining % pasted_chars != 0:
+            # paste current clipboard
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 1
+        else:
+            # copyall
+            clipboard = pasted_chars
+            # paste
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 2
+
+    # if got the desired result
+    if pasted_chars == n:
+        return counter
+    else:
+        return 0
